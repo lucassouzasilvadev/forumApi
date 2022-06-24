1c8f126dcd749ca27158dd2d9a716026d9e3b4cc
lucassouzasilvadev <lucasdrop169@gmail.com>
2022-06-24 03:46:27 -0300

    FEAT: adicionando estrutura inicial do projeto

---
+++ forum-api/src/main/kotlin/br/com/forum/forumapi/ForumApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.forum.forumapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForumApiApplication>(*args)
+}
---
+++ forum-api/src/test/kotlin/br/com/forum/forumapi/ForumApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.forum.forumapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
