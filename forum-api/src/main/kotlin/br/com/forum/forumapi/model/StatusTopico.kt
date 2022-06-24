33ceab419ae1579e224ca00ea4b43b30099af66b
lucassouzasilvadev <lucasdrop169@gmail.com>
2022-06-24 04:14:02 -0300

    FEAT: criando classes de dominio do forum

---
+++ forum-api/src/main/kotlin/br/com/forum/forumapi/model/StatusTopico.kt
@@ -0,0 +1,8 @@
+package br.com.forum.forumapi.model
+
+enum class StatusTopico {
+    NAO_RESPONDIDO,
+    NAO_SOLUCIONADO,
+    SOLUCIONADO,
+    FECHADO
+}
---
+++ forum-api/src/main/kotlin/br/com/forum/forumapi/model/Usuario.kt
@@ -0,0 +1,7 @@
+package br.com.forum.forumapi.model
+
+data class Usuario(
+    val id: Long? = null,
+    val nome: String,
+    val email: String
+)
