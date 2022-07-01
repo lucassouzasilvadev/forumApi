8a866a6c05fefa34d6c5bd999638742a50c6b56e
lucassouzasilvadev <lucasdrop169@gmail.com>
2022-07-01 02:20:18 -0300

    FEAT(TopicoRepository): criando find na camada de repository para buscar topico pelo nome do curso

--- forum-api/src/main/kotlin/br/com/forum/forumapi/repository/TopicoRepository.kt
+++ forum-api/src/main/kotlin/br/com/forum/forumapi/repository/TopicoRepository.kt
@@ -4,5 +4,5 @@ import br.com.forum.forumapi.model.Topico
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TopicoRepository: JpaRepository<Topico, Long> {
-
+    fun findByCursoNome(nomeCurso: String?): List<Topico>
 }
