1467b366a2eb2649423f322fa51ca58fe5b0cb22
lucassouzasilvadev <lucasdrop169@gmail.com>
2022-07-01 03:12:16 -0300

    REFACTOR(TopicoRepository.kt): trocando retorno do find de List para Page

--- forum-api/src/main/kotlin/br/com/forum/forumapi/repository/TopicoRepository.kt
+++ forum-api/src/main/kotlin/br/com/forum/forumapi/repository/TopicoRepository.kt
@@ -1,8 +1,10 @@
 package br.com.forum.forumapi.repository
 
 import br.com.forum.forumapi.model.Topico
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TopicoRepository: JpaRepository<Topico, Long> {
-    fun findByCursoNome(nomeCurso: String?): List<Topico>
+    fun findByCursoNome(nomeCurso: String?, paginacao: Pageable): Page<Topico>
 }
