fd6653485908cb9d1e010ac17165036650409b53
lucassouzasilvadev <lucasdrop169@gmail.com>
2022-07-01 02:22:32 -0300

    REFACTOR(v3__create_table_topico.sql): ajustando nome da coluna da tabela topico

--- forum-api/src/main/resources/db/migration/V3__create_table_topico.sql
+++ forum-api/src/main/resources/db/migration/V3__create_table_topico.sql
@@ -2,7 +2,7 @@ CREATE TABLE topico(
     id SERIAL NOT NULL,
     titulo VARCHAR(50) NOT NULL,
     mensagem VARCHAR(300) NOT NULL,
-    dataCriacao TIMESTAMP NOT NULL,
+    data_criacao TIMESTAMP NOT NULL,
     status VARCHAR(30) NOT NULL,
     curso_id BIGINT NOT NULL,
     autor_id BIGINT NOT NULL,
