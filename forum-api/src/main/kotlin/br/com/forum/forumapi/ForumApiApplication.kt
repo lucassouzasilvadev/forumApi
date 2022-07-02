facc5f40e514371e03f5bf39140ffd793a304e82
lucassouzasilvadev <lucasdrop169@gmail.com>
2022-07-01 23:43:00 -0300

    FEAT(ForumApiApplication.kt): adicinando permisao para utilizar o cache na classe main do projeto

--- forum-api/src/main/kotlin/br/com/forum/forumapi/ForumApiApplication.kt
+++ forum-api/src/main/kotlin/br/com/forum/forumapi/ForumApiApplication.kt
@@ -2,8 +2,10 @@ package br.com.forum.forumapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class ForumApiApplication
 
 fun main(args: Array<String>) {
